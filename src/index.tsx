d2dfef1c43893ab13017648fa22d9ced93607489
osdnk <micosa97@gmail.com>
2020-02-29 23:46:38 +0100

    feat: add platform-specific things

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
-import { requireNativeComponent } from 'react-native';
+import { requireNativeComponent, Platform } from 'react-native';
 
-export default requireNativeComponent('ModalView');
+export default Platform.select({
+  ios: requireNativeComponent('ModalView'),
+  get default() {
+    console.warn(
+      `BottomSheet is available only on iOS. Consider using 'cesardeazevedo/react-native-bottom-sheet-behavior' on Android or reanimated-bottom-sheet on web`
+    );
+    return null;
+  },
+});
